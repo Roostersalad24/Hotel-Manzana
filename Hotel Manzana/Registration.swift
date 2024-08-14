2626784abdae6060d905ac11c3690e8971e08ab5
Roostersalad24 <AndrewJohnsonmga@gmail.com>
2024-08-13 22:29:04 -0500

    Initial Commit

---
+++ Hotel Manzana/Registration.swift
@@ -0,0 +1,22 @@
+//
+//  Registration.swift
+//  Hotel Manzana
+//
+//  Created by Andrew Johnson on 1/4/24.
+//
+
+import Foundation
+
+struct Registration {
+    var firstName: String
+    var lastName: String
+    var emailAddress: String
+    
+    var checkInDate: Date
+    var checkOutDate: Date
+    var numberOfAdults: Int
+    var numberOfChildren: Int
+    
+    var wifi: Bool
+    var roomType: RoomType
+}
